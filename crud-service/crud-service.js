4e4cf32477a7ebc836e670635cda83b428873748
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-23 02:15:09 -0400

    mongoose v6 upgrade

--- crud-service/crud-service.js
+++ crud-service/crud-service.js
@@ -1,185 +1,180 @@
 const express = require("express");
 const { executeDomain } = require("../utils/utils");
 
-//c,r,u,d is domain logic hooks (before creation, read, update or delete);
-//params is something we use to attach this resource to (for example, the current user id so we don't return resources for other users)
 const crudService = function ({
   Model,
   crudDomainLogic: { create, read, update, del, search },
 }) {
-  var apiRoutes = express.Router();
+  const apiRoutes = express.Router();
 
-  apiRoutes.get("/", function (req, res) {
-    let { criteria, isPermitted, populate, onResponse, exclude } =
-      executeDomain(req, res, read);
-    let query;
-    if (criteria) {
-      query = criteria.query;
+  apiRoutes.get("/", async (req, res) => {
+    try {
+      let { criteria, isPermitted, populate, onResponse, exclude } = executeDomain(req, res, read);
+      let query = criteria ? criteria.query : {};
+      if (!populate) {
+        populate = "";
+      }
+      if (!isPermitted) {
+        return res.status(409).send({
+          message: `You are not authorized to read ${Model.modelName}s`,
+        });
+      }
+      let exclusionList = exclude && exclude.map((ex) => `-${ex}`).join(" ");
+      const data = await Model.find(query)
+        .sort("-createdAt")
+        .populate(Array.isArray(populate) ? populate.join(" ") : "")
+        .select(exclusionList)
+        .exec();
+
+      if (onResponse) {
+        onResponse({ data, count: data.length }, req, res);
+      } else {
+        res.status(200).send({ data, count: data.length });
+      }
+    } catch (err) {
+      return res.status(500).send({ message: `Database connection error: ${err.message}` });
     }
-    if (!populate) {
-      populate = "";
-    }
-    if (!isPermitted) {
-      return res.status(409).send({
-        message: `You are not authorized to read ${Model.modelName}s`,
-      });
-    }
-    let exclusionList = exclude && exclude.map((ex) => `-${ex}`).join(" ");
-    Model.find(query)
-      .sort("-createdAt")
-      .populate((Array.isArray(populate) && populate.join(" ")) || "")
-      .select(exclusionList)
-      .exec((err, data) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        onResponse
-          ? onResponse({ data: data, count: data.length }, req, res)
-          : res.status(200).send({ data: data, count: data.length });
-      });
   });
 
-  apiRoutes.get("/paginate/:page/:limit", (req, res) => {
-    let { criteria, isPermitted, populate, onResponse, exclude } =
-      executeDomain(req, res, read);
-    let { query } = criteria;
-    let { page, limit } = req.params;
-    page = parseInt(page);
-    limit = parseInt(limit);
-    if (!populate) {
-      populate = "";
-    }
-    if (!isPermitted) {
-      return res.status(409).send({
-        message: `You are not authorized to read ${Model.modelName}s`,
-      });
-    }
-    let exclusionList = exclude && exclude.map((ex) => `-${ex}`).join(" ");
-    Model.count({}, function (err, count) {
-      if (err) {
-        return res.status(500).send(err);
+  apiRoutes.get("/paginate/:page/:limit", async (req, res) => {
+    try {
+      let { criteria, isPermitted, populate, onResponse, exclude } = executeDomain(req, res, read);
+      let { query } = criteria;
+      let { page, limit } = req.params;
+      page = parseInt(page);
+      limit = parseInt(limit);
+      if (!populate) {
+        populate = "";
       }
-      Model.paginate(
+      if (!isPermitted) {
+        return res.status(409).send({
+          message: `You are not authorized to read ${Model.modelName}s`,
+        });
+      }
+      let exclusionList = exclude && exclude.map((ex) => `-${ex}`).join(" ");
+      const count = await Model.countDocuments(query).exec();
+      const data = await Model.paginate(
         query,
         {
           page,
           limit,
           select: exclusionList,
           sort: `-createdAt`,
-        },
-        (err, data) => {
-          if (err) {
-            return res.status(500).send(err);
-          }
-          onResponse
-            ? onResponse({ data: data.docs, count }, req, res)
-            : res.status(200).send({ data: data.docs, count });
         }
       );
-    });
+
+      if (onResponse) {
+        onResponse({ data: data.docs, count }, req, res);
+      } else {
+        res.status(200).send({ data: data.docs, count });
+      }
+    } catch (err) {
+      return res.status(500).send({ message: `Database connection error: ${err.message}` });
+    }
   });
 
-  apiRoutes.post("/create", function (req, res) {
-    let { isPermitted, onResponse } = executeDomain(req, res, create);
-    let newModel = new Model(req.body.model);
+  apiRoutes.post("/create", async (req, res) => {
+    try {
+      let { isPermitted, onResponse } = executeDomain(req, res, create);
+      let newModel = new Model(req.body.model);
 
-    if (Model.joiValidate) {
-      let { error } = Model.joiValidate(newModel);
-      if (error) {
-        return res.status(409).send({
-          message: `error validating your input ${error}`,
-        });
-      }
-    }
-
-    if (!isPermitted) {
-      return res.status(409).send({
-        message: `You are not authorized to create this ${Model.modelName}`,
-      });
-    }
-
-    newModel.save((err) => {
-      if (err) {
-        return res.status(409).send({ message: err.message });
-      }
-      onResponse
-        ? onResponse(newModel, req, res)
-        : res.status(200).send(newModel);
-    });
-  });
-
-  apiRoutes.put("/", (req, res) => {
-    //take the consted Model, format it and add it to the Models collection
-    let { criteria, isPermitted, onResponse } = executeDomain(req, res, update);
-    if (!isPermitted) {
-      return res.status(409).send({
-        message: `You are not authorized to update this ${Model.modelName}`,
-      });
-    }
-
-    let newModel = Object.assign({}, req.body.model);
-
-    if (Model.joiValidate) {
-      let { error } = Model.joiValidate(newModel);
-      if (error) {
-        return res.status(409).send({
-          message: `Error validating your input ${error}`,
-        });
-      }
-    }
-
-    Model.findOneAndUpdate(
-      { _id: newModel._id, ...criteria },
-      newModel,
-      {
-        upsert: false,
-      },
-      function (err, doc) {
-        if (err) return res.send(500, { error: err });
-        onResponse
-          ? onResponse(newModel, req, res)
-          : res.status(200).send(newModel);
-      }
-    );
-  });
-
-  apiRoutes.delete("/:_id", (req, res) => {
-    let requestModelID = req.params._id;
-    let { criteria, isPermitted } = executeDomain(req, res, del);
-    if (!isPermitted) {
-      return res.status(409).send({
-        message: `You are not authorized to delete this ${Model.modelName}`,
-      });
-    }
-    Model.find({
-      _id: requestModelID,
-      ...criteria,
-    })
-      .remove()
-      .exec((err) => {
-        if (err) {
-          return res.status(500).send(err);
+      if (Model.joiValidate) {
+        let { error } = Model.joiValidate(newModel);
+        if (error) {
+          return res.status(409).send({
+            message: `Error validating your input ${error}`,
+          });
         }
-        res.status(200).send();
-      });
+      }
+
+      if (!isPermitted) {
+        return res.status(409).send({
+          message: `You are not authorized to create this ${Model.modelName}`,
+        });
+      }
+
+      await newModel.save();
+
+      if (onResponse) {
+        onResponse(newModel, req, res);
+      } else {
+        res.status(200).send(newModel);
+      }
+    } catch (err) {
+      return res.status(500).send({ message: `Database connection error: ${err.message}` });
+    }
   });
 
-  apiRoutes.post("/search", (req, res) => {
-    let query = req.body.query;
-    let { criteria, isPermitted, onResponse } = executeDomain(req, res, search);
-    if (!isPermitted) {
-      return res.status(409).send({
-        message: `You are not authorized to search ${Model.modelName}s`,
-      });
-    }
-    Model.find({ ...query, ...criteria }).exec((err, results) => {
-      if (err) {
-        return res.status(500).send(err);
+  apiRoutes.put("/", async (req, res) => {
+    try {
+      let { criteria, isPermitted, onResponse } = executeDomain(req, res, update);
+      if (!isPermitted) {
+        return res.status(409).send({
+          message: `You are not authorized to update this ${Model.modelName}`,
+        });
       }
-      return onResponse
-        ? onResponse(results, req, res)
-        : res.status(200).send(results);
-    });
+
+      let newModel = Object.assign({}, req.body.model);
+
+      if (Model.joiValidate) {
+        let { error } = Model.joiValidate(newModel);
+        if (error) {
+          return res.status(409).send({
+            message: `Error validating your input ${error}`,
+          });
+        }
+      }
+
+      const updatedModel = await Model.findOneAndUpdate(
+        { _id: newModel._id, ...criteria },
+        newModel,
+        { new: true, upsert: false }
+      ).exec();
+
+      if (onResponse) {
+        onResponse(updatedModel, req, res);
+      } else {
+        res.status(200).send(updatedModel);
+      }
+    } catch (err) {
+      return res.status(500).send({ message: `Database connection error: ${err.message}` });
+    }
+  });
+
+  apiRoutes.delete("/:_id", async (req, res) => {
+    try {
+      let requestModelID = req.params._id;
+      let { criteria, isPermitted } = executeDomain(req, res, del);
+      if (!isPermitted) {
+        return res.status(409).send({
+          message: `You are not authorized to delete this ${Model.modelName}`,
+        });
+      }
+      await Model.deleteOne({ _id: requestModelID, ...criteria }).exec();
+      res.status(200).send();
+    } catch (err) {
+      return res.status(500).send({ message: `Database connection error: ${err.message}` });
+    }
+  });
+
+  apiRoutes.post("/search", async (req, res) => {
+    try {
+      let query = req.body.query;
+      let { criteria, isPermitted, onResponse } = executeDomain(req, res, search);
+      if (!isPermitted) {
+        return res.status(409).send({
+          message: `You are not authorized to search ${Model.modelName}s`,
+        });
+      }
+      const results = await Model.find({ ...query, ...criteria }).exec();
+      if (onResponse) {
+        onResponse(results, req, res);
+      } else {
+        res.status(200).send(results);
+      }
+    } catch (err) {
+      return res.status(500).send({ message: `Database connection error: ${err.message}` });
+    }
   });
 
   return apiRoutes;
